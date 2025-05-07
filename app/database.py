c7fe1be22ab9925231f6949995d2f7c82d0c5562
Erick Alvarez <42850106+becker7000@users.noreply.github.com>
2025-05-06 18:45:03 -0600

    Delete app directory

--- app/database.py
+++
@@ -1,6 +0,0 @@
-from typing import List
-from .models import Producto
-
-# Esta lista actúa como nuestra "base de datos en memoria"
-# Aquí se almacenan los productos agregados durante la ejecución.
-menu: List[Producto] = []
